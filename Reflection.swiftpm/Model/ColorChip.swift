2b6645b9093eb0e8eb719fce82491509175a8a04
JOY <jypjoy@gmail.com>
2024-02-13 13:36:08 +0900

    feat: Add ColorChip Entity

--- Reflection.swiftpm/Model/ColorChip.swift
+++ Reflection.swiftpm/Model/ColorChip.swift
@@ -1,1 +1,26 @@
 import Foundation
+import CoreData
+
+@objc(ColorChip)
+class ColorChip: NSManagedObject {
+    @NSManaged var identifier: UUID
+    @NSManaged var colorName: String
+    @NSManaged var colorList: [String]
+    @NSManaged var memories: NSSet
+}
+
+extension ColorChip: Identifiable {
+    var id: UUID {
+        identifier
+    }
+    
+    /// Add memories to the ColorChip
+    func addMemory(values: NSSet) {
+        values.forEach { self.mutableSetValue(forKey: "memories").add($0) }
+    }
+    
+    /// Remove memories from the Colorchip
+    func removeMemory(values: NSSet) {
+        values.forEach { self.mutableSetValue(forKey: "memories").remove($0) }
+    }
+}
