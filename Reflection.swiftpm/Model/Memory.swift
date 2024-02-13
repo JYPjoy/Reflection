4d95f5039b1aec32bba5b1619124b11fa8a51479
JOY <jypjoy@gmail.com>
2024-02-13 13:42:24 +0900

    feat: Add Memory Entity

--- Reflection.swiftpm/Model/Memory.swift
+++ Reflection.swiftpm/Model/Memory.swift
@@ -1,1 +1,17 @@
 import Foundation
+import CoreData
+
+@objc(Memory)
+class Memory: NSManagedObject {
+    @NSManaged var identifier: UUID
+    @NSManaged var picture: Data?
+    @NSManaged var title: String
+    @NSManaged var reflection: String
+    @NSManaged var colorChip: NSSet /// colorchip과의 관계 정의를 위함
+}
+
+extension Memory: Identifiable {
+    var id: UUID {
+        identifier
+    }
+}
