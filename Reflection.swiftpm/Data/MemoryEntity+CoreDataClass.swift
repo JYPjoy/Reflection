aae3c972f4a0c57bba669dbe530938a692d5ea26
JOY <jypjoy@gmail.com>
2024-02-16 11:54:06 +0900

    refactor: MemoryEntity

--- Reflection.swiftpm/Data/MemoryEntity+CoreDataClass.swift
+++ Reflection.swiftpm/Data/MemoryEntity+CoreDataClass.swift
@@ -2,16 +2,14 @@ import Foundation
 import CoreData
 
 @objc(Memory)
-class MemoryEntity: NSManagedObject {
+class MemoryEntity: NSManagedObject, Identifiable {
     @NSManaged var identifier: UUID
     @NSManaged var picture: Data?
     @NSManaged var title: String
     @NSManaged var reflection: String
-    @NSManaged var colorChip: NSSet /// colorchip과의 관계 정의를 위함
+    @NSManaged var colorChip: ColorChipEntity
     // 날짜, 위치 정보 추가될 수도
-}
-
-extension MemoryEntity: Identifiable {
+    
     var id: UUID {
         identifier
     }
