0819c49cea3d170a0b8f1c8139080f2ee278c8b3
JOY <jypjoy@gmail.com>
2024-02-16 12:35:07 +0900

    refactor: Refactor ColorChip Entity

--- Reflection.swiftpm/Data/ColorChipEntity+CoreDataClass.swift
+++ Reflection.swiftpm/Data/ColorChipEntity+CoreDataClass.swift
@@ -2,15 +2,13 @@ import Foundation
 import CoreData
 
 @objc(ColorChip)
-class ColorChipEntity: NSManagedObject {
+class ColorChipEntity: NSManagedObject, Identifiable {
     @NSManaged var identifier: UUID
     @NSManaged var colorName: String
     @NSManaged var colorCount: Int
     @NSManaged var colorList: [String]
-    @NSManaged var memories: Set<MemoryEntity>
-}
-
-extension ColorChipEntity: Identifiable {
+    @NSManaged var memories: NSSet
+    
     var id: UUID {
         identifier
     }
