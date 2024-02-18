403683259cc2b299a2ca9e589731c33fa44d1643
JOY <jypjoy@gmail.com>
2024-02-18 17:05:41 +0900

    refacotr: Rearrange the UI component

--- Reflection.swiftpm/Presentation/ColorChip/CreateColorChipView.swift
+++ Reflection.swiftpm/Presentation/ColorChip/CreateColorChipView.swift
@@ -1,5 +1,7 @@
 import SwiftUI
 
+// TODO: 버튼 비활성화, 더 친절한 form이 되도록 안내 문구 함께 나오도록 하기
+// 컴포넌트 크기 조정 필요
 struct CreateColorChipView: View {
     @Environment(\.managedObjectContext) var viewContext
     @Environment(\.dismiss) var dismiss
@@ -11,6 +13,7 @@ struct CreateColorChipView: View {
     
     var body: some View {
         VStack {
+            // MARK: - Form
             Form {
                 // SECTION1
                 Section {
@@ -35,7 +38,6 @@ struct CreateColorChipView: View {
                         } label: {
                             Text("Generate the ColorPickers")
                                 .frame(maxWidth: .infinity, minHeight: 20)
-                                .cornerRadius(10)
                         }.mainButton()
                     }
                 } header: {
@@ -43,9 +45,8 @@ struct CreateColorChipView: View {
                 }
                 
                 
-                // SECTION 3
+                // SECTION 3 (리팩터링 하기)
                 Section {
-                    // ColorPicker 배열을 동적으로 생성
                     ForEach(0..<colorCount, id: \.self) { idx in
                         ColorPicker("Selected Color \(idx + 1)",
                                     selection: Binding(get: {
@@ -61,15 +62,21 @@ struct CreateColorChipView: View {
                 } header: {
                     Text("Select the colors")
                 }
-                
-                Button {
-                    print(colorList)
-                    self.dismiss()
-                } label: {
-                    Text("Make a Color Chip")
-                }
-                .blackButton()
             }
+            
+            // MARK: - Button
+            Button {
+                print(colorList)
+                self.dismiss()
+            } label: {
+    
+                    Text("Make a Color Chip")
+                    .frame(maxWidth: .infinity, minHeight: 20)
+       
+            }
+            //.diabled()
+            .blackButton()
+            .padding(30)
         }
         .navigationTitle(Text("Create a New Color Chip"))
     }
