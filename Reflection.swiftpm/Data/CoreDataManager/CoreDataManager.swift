65147fc357afa1fbb9a5e81ed001c32ce06d28f1
JOY <jypjoy@gmail.com>
2024-02-21 09:31:50 +0900

    fix: colorChip 업데이트 후 읽히지 않던 문제 해결

--- Reflection.swiftpm/Data/CoreDataManager/CoreDataManager.swift
+++ Reflection.swiftpm/Data/CoreDataManager/CoreDataManager.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 import Combine
 import CoreData
+import Log
 
 // MARK: - Managable
 protocol ColorChipManagable {
@@ -151,6 +152,7 @@ extension CoreDataManager: ColorChipManagable {
                     colorChipEntity.addToMemories(NSSet(array:self.fetchMemoryEntity(of: colorChip.memories)))
                     
                     try self.backgroundContext.save()
+                    promise(.success(colorChipEntity))
                 } catch {
                     promise(.failure(.update))
                 }
