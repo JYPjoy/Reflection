fd7183d2b9a56322156079a1f68f4bf740676c97
JOY <jypjoy@gmail.com>
2024-02-21 16:07:40 +0900

    refactor: 버튼 라벨 변경

--- Reflection.swiftpm/Presentation/ColorChip/View/ColorChipFormView.swift
+++ Reflection.swiftpm/Presentation/ColorChip/View/ColorChipFormView.swift
@@ -8,7 +8,7 @@ struct ColorChipFormView: View {
     //현재 뷰 구성 위함
     @State private var colorChipToEdit: ColorChip?
     @State private(set) var navigationTitle = "Create a New Color Chip"
-    @State private(set) var buttonText = "Make a new Color Chip"
+    @State private(set) var buttonText = "Create"
     
     @State private(set) var colorName: String = ""
     @State private(set) var colorList: Color = Color(hex: "#F8D749")
