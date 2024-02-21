d2ee5c52da713bb03fbbe81593e80a7423c0a580
JOY <jypjoy@gmail.com>
2024-02-21 15:54:51 +0900

    chore: Package 권한 설정

--- Reflection.swiftpm/Package.swift
+++ Reflection.swiftpm/Package.swift
@@ -31,6 +31,12 @@ let package = Package(
                 .landscapeRight,
                 .landscapeLeft,
                 .portraitUpsideDown(.when(deviceFamilies: [.pad]))
+            ],
+            capabilities: [
+                .fileAccess(.pictureFolder, mode: .readOnly),
+                .fileAccess(.userSelectedFiles, mode: .readOnly),
+                .photoLibrary(purposeString: "Select a photo to remember this color"),
+                .photoLibraryAdd(purposeString: "Select a photo to remember this color")
             ]
         )
     ],
