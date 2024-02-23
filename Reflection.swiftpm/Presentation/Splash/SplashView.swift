4ab0e5938b49376d7d72f228de9998dc0fd1e544
JOY <jypjoy@gmail.com>
2024-02-24 01:58:03 +0900

    chore: Splash 시간 재조정

--- Reflection.swiftpm/Presentation/Splash/SplashView.swift
+++ Reflection.swiftpm/Presentation/Splash/SplashView.swift
@@ -13,7 +13,7 @@ struct SplashView: View {
             }
         }
         .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
                 withAnimation {
                     self.isActive = true
                 }
