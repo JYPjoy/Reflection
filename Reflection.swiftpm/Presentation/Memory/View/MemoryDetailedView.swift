eba5dfdb2779f2dd93366760e24655bfe75ecaa6
JOY <jypjoy@gmail.com>
2024-02-25 18:36:46 +0900

    refactor: MemoryDetailedView UI 리팩터링 완료

--- Reflection.swiftpm/Presentation/Memory/View/MemoryDetailedView.swift
+++ Reflection.swiftpm/Presentation/Memory/View/MemoryDetailedView.swift
@@ -17,59 +17,76 @@ struct MemoryDetailedView: View {
     }
     
     var body: some View {
-        ZStack {
-            Color.Text.text30.ignoresSafeArea()
-            VStack(spacing: 10) {
-                // Section1
-                ZStack{
-                    Rectangle().frame(height: 80).foregroundStyle(.white)
-                    HStack {
-                        Text(memory.title)
-                            .font(.largeTitle)
-                            .foregroundStyle(Color.black).bold()
-                        Spacer()
-                        Text(dateString)
-                            .font(.callout)
-                            .foregroundStyle(Color.black)
-                    }
-                    .padding([.leading, .trailing], 30)
-                }
-                
-                // Section2
-                ZStack {
-                    Rectangle().frame(height: 400).foregroundStyle(.white)
-                    if let picture = memory.picture, let pictureImage = UIImage(data: picture) {
-                        Image(uiImage: pictureImage)
-                            .resizable()
-                            .aspectRatio(1, contentMode: .fit)
-                            .frame(width: 350, height: 350)
-                            .clipped()
-                    } else {
-                        ZStack {
-                            Rectangle()
-                                .foregroundColor(.Text.text90)
-                                .aspectRatio(1, contentMode: .fit)
-                            Image(systemName: "photo")
+        ScrollView {
+            ZStack {
+                Color(hex: colorChip.colorList)
+                VStack(spacing:0) {
+                    Spacer().frame(height: 20)
+                    
+                    // Section1
+                    ZStack {
+                        Rectangle()
+                            .fill(Color.white)
+                            .frame(height: 400)
+                            .border(Color.black, width: 0.5)
+                        if let picture = memory.picture, let pictureImage = UIImage(data: picture) {
+                            Image(uiImage: pictureImage)
                                 .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 200, height: 200)
-                                .foregroundColor(.Main.main10)
+                                .aspectRatio(1, contentMode: .fit)
+                                .frame(width: 350, height: 350)
+                                .clipped()
+                                .accessibilityLabel(Text(memory.title))
+                        } else {
+                            ZStack {
+                                Rectangle()
+                                    .foregroundColor(.Text.text90)
+                                    .aspectRatio(1, contentMode: .fit)
+                                Image(systemName: "photo")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 200, height: 200)
+                                    .foregroundColor(.Main.main10)
+                                    .accessibilityLabel("Photo")
+                            }
                         }
                     }
-                }
-                
-                // Section3
-                ZStack{
-                    Rectangle().frame(height: 350).foregroundStyle(.white)
-                    Text(memory.reflection)
                     
-                }
-                Spacer()
-            }
+                    // Section2
+                    ZStack{
+                        Rectangle()
+                            .fill(Color.white)
+                            .frame(height: 100)
+                            .border(Color.black, width: 0.5)
+              
+                        VStack {
+                            Text(memory.title)
+                                .font(.largeTitle)
+                                .foregroundStyle(Color.black).bold()
+                            
+                            Text(dateString)
+                                .font(.callout)
+                                .foregroundStyle(Color.black)
+                        }
+                        .padding([.leading, .trailing], 30)
+                    }
+                    
+                    // Section3
+                    ZStack{
+                        Rectangle()
+                            .fill(Color.white)
+                        Text(memory.reflection).font(.body).padding()
+                    }
+                    .border(Color.black, width: 0.5)
+                    //.padding()
             
-        }
-        .onAppear {
-            dateString = MemoryDetailedView.dateFormatter.string(for: memory.date) ?? ""
+                }
+                .padding([.leading, .trailing], 200)
+                .padding([.top, .bottom], 40)
+                Spacer().frame(height: 40)
+            }
+            .onAppear {
+                dateString = MemoryDetailedView.dateFormatter.string(for: memory.date) ?? ""
+            }
         }
     }
 }
