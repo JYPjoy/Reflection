935eba2cb4f6b322c98f8babf539dae585889a37
JOY <jypjoy@gmail.com>
2024-02-28 16:48:47 +0900

    chore: Migrate to xcode project

--- Reflection.swiftpm/Application/ReflectionApp.swift
+++ Reflection.swiftpm/Application/ReflectionApp.swift
@@ -1,7 +1,14 @@
+//
+//  ReflectionApp.swift
+//  Reflection
+//
+//  Created by Joy on 2/28/24.
+//
+
 import SwiftUI
 
 @main
-struct MyApp: App {
+struct ReflectionApp: App {
     var body: some Scene {
         WindowGroup {
             SplashView()
